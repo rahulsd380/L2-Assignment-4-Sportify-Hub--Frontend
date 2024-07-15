5aad5127ef9d515db6737ad357966210c9d1f1ea
Rahul Sutradhar <rahulsd380@gmail.com>
2024-07-14 21:27:03 -0300

    deployed backend api added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://sportify-hub-backend.vercel.app' }),
   tagTypes: ["products", "cart", "order"],
   endpoints: (builder) => ({
     getAllProducts: builder.query({
